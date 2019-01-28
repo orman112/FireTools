65e5fb88b5f7a139746e221ac8d4615604c5cc82
Orman, Clayton <CDO8081@yum.com>
2019-01-28 12:18:52 -0500

    Add todo comments for future work items

--- FireTools.App/Program.cs
+++ FireTools.App/Program.cs
@@ -7,6 +7,33 @@ namespace FireTools.App
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //TODO: Calculate the following:
+            //-> Savings Rate
+                //Three Options:
+                    //Total Savings (yearly) / (Gross/Taxes)
+                    //Total Savings / Net Income (gross less taxes)
+                    //Total Savings (pre & post tax) / (After tax takehome pay + pre-tax contributions)
+
+            //-> Retirement Calculator
+
+            //-> Compound Interest Calculator
+                //This could be used to see how much you could save for big items like college
+                //by not spending on daily smaller items like a cup of coffee ($3.50 per day)
+
+            //-> Calculate Yearly Spending and amount of money needed to retire
+                //This assumes 4% withdraws (25x yearly spending)
+
+            //-> Mortgage Amortization Calculator
+                //Calculate how early you can pay off a mortgage with extra payments
+                //Utilize and API?
+
+            //Calculate % to Financial Independence
+                //Amount saved (dedicated to retirement) / Target Amount
+
+            //Calculate % to Retire Early
+                //Could be the same amount as FI above, but some may want to work a few more years
+                //for a better margin of safety
         }
     }
 }
