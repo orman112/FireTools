f4692828cffed68608b3c62404701bc466550d7f
Orman, Clayton <CDO8081@yum.com>
2019-02-22 11:54:07 -0500

    Add tests for expenses service

---
+++ FireTools.Tests/ExpensesTests.cs
@@ -0,0 +1,31 @@
+﻿using FireTools.Utilities;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FireTools.Tests
+{
+    public class ExpensesTests
+    {
+        [Test]
+        public void CupOfCoffee_ShouldSaveFifteenThousandDollars()
+        {
+            //$4 cup of coffee equals $20 a week
+            var expensesService = new ExpensesService(20);
+            var amountSavedByNotSpending = expensesService.CalculateWeeklyExpenses();
+
+            Assert.AreEqual(15040, amountSavedByNotSpending);
+        }
+
+        [Test]
+        public void ProfessionalHaircut_ShouldSaveSeventeenThousandDollars()
+        {
+            //$100 haircut each month
+            var expensesService = new ExpensesService(100);
+            var amountSavedByNotSpending = expensesService.CalculateMonthlyExpenses();
+
+            Assert.AreEqual(17300, amountSavedByNotSpending);
+        }
+    }
+}
